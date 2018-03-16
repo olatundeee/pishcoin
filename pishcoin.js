cbc4c9f7aa1b15e74c72f5ab84af6320bbd5b201
Olatunde Oladunni <wichitawebservices@gmail.com>
2018-03-16 17:21:22 +0100

    first commit

---
+++ pishcoin.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	  $(".mouse-in-1").mouseenter(function(){
+	        $(.sec-list-1).show();
+	    }
+});
+
+$(document).ready(function(){
+	  $(".mouse-in-1").mouseenter(function(){
+	        $(.sec-list-2).show();
+	    }
+});
